ec2fdb8d0051dd1e58da7ff6004c75e2cb9af17b
mini <mini@qq.com>
2023-02-24 11:46:07 +0800

    master 01 commit

--- src/test/a.java
+++ src/test/a.java
@@ -4,7 +4,6 @@ public class a {
     public static void main(String[] args){
         System.out.println("hello world");
         System.out.println("hello git!");
-        System.out.println("hello hot-fix");
         System.out.println("hello master");
     }
 
